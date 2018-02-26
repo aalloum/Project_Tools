1b0510731c9168c0f5a283ae7afbca76e2804799
ALLOUM <alloum1abderrahim@gmail.com>
2018-02-26 12:58:55 +0100

    Spring Security with angular 4

---
+++ SpringSecurity_JWT_Angular_4/jwt-spring-sec/src/main/java/com/lucifer/entities/Task.java
@@ -0,0 +1,21 @@
+package com.lucifer.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor 
+@NoArgsConstructor
+public class Task {
+	@Id 
+	@GeneratedValue
+	private Long id;
+	private String taskName;
+	
+}
---
+++ SpringSecurity_JWT_Angular_4/jwt-spring-sec/src/main/java/com/lucifer/web/RegisterForm.java
@@ -0,0 +1,11 @@
+package com.lucifer.web;
+
+import lombok.Data;
+
+@Data
+public class RegisterForm {
+
+	private String username;
+	private String password;
+	private String repassword;
+}
